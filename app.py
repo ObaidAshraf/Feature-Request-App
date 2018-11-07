003db1c6e5dd75deae75bd61327031ebef7fac21
ObaidAshraf <ubaidashraf91@gmail.com>
2018-11-07 23:49:51 +0500

    First commit. Added directories and added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, url_for, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    return "Hello!"
+
+
+app.run(debug = True)
